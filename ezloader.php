113e38185f4e56f34d3418f70e461861917796ce
Weng Fei Fung <siphon880g@gmail.com>
2016-02-08 02:01:25 -0800

    Ezloader: Bug fixed (Handlebars not loading)

--- ezloader.php
+++ ezloader.php
@@ -49,7 +49,7 @@ function load_jqui() {
 function load_bs() {
     ob_start();
     ?><link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"/>
-<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js">
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <?php
     echo ob_get_clean();
 }
